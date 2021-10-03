aa38301be7c9b7944ea17843212ba88a422726d9
unknown <lipkap1@gmail.com>
2021-10-03 12:52:52 +0200

    Added few functions and tests

--- tests_catalog_downloader.py
+++ tests_catalog_downloader.py
@@ -1,8 +1,16 @@
-import unittest
+from unittest import mock,TestCase
 from Main import Cataloge_downloader
 
-class MyTestCase(unittest.TestCase):
+
+class MyTestCase(TestCase):
+
+
     def test_sn_input(self):
-        self.assertEqual(Cataloge_downloader.sn_input())
+        with mock.patch('builtins.input', return_value = 'test'):
+            self.assertEqual(Cataloge_downloader.sn_input(self),'test')
+    def test_log_in(selfs):
+        pass
+    def page_open(self):
+
 if __name__ == '__main__':
     unittest.main()
