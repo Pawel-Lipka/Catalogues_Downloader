8e9e4784b4dae8dadcb9abe147785c61f4c35ece
Lipka Pawel <plipka@frigoglass.com>
2021-11-09 13:16:57 +0100

    add constans file

---
+++ constants.py
@@ -0,0 +1,7 @@
+import base64
+FRIGO_SHOP_LOGIN_PAGE = "https://parts.frigoserve.com/shop/app?__bk_&__windowid=CCS428213376&__rid=RIH1634113424794#'2V10C9D9248E4C6405"
+USER_FRS_RO = base64.b64decode('dmxhZA==').decode('utf-8')
+PASSWORD_FRS_RO = base64.b64decode('dmxhZDAw').decode('utf-8')
+USER_FRG_SHOP = base64.b64decode('UEw=').decode('utf-8')
+PASSSWORD_FRG_SHOP = base64.b64decode('UExGUkdIVUxVJCVe').decode('utf-8')
+PATH = r'C:\Users\plipka\Desktop\AutoFrigo\Catalogues_Downloader\chromedriver'
